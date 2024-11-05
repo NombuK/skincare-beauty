999c58779d8b8094d109cc0e155b583dff4bd7de
Nombulelo <nombulelokhumalo48@gmail.com>
2024-11-05 09:57:24 +0200

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+function App() {
+  return (
+    <>
+      <h1 className="font-nova text-pink-600">SkinCare Beauty</h1>
+      <p className="font-algeria text-pink-400">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Facere omnis
+        aspernatur porro possimus sunt rerum maxime esse, mollitia iste optio
+        placeat autem, eligendi sequi deleniti itaque beatae quasi saepe in?
+      </p>
+    </>
+  );
+}
+
+export default App;
